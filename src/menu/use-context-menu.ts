403571a6de5f8792164dc8af0cfaf01ed891f6a8
Shawn Harvell <sharvell@jbecker.com>
2020-01-25 15:10:26 -0500

    delint click outside hook

--- src/menu/use-context-menu.ts
+++ src/menu/use-context-menu.ts
@@ -1,13 +1,24 @@
+import React from 'react';
 import { useEffect, useCallback, useState } from 'react';
 
-const useContextMenu = (outerRef: any, onSetOpen: (open: boolean) => void) => {
+export type ContextMenuPosition = {
+    xPos: string;
+    yPos: string;
+};
+
+const useContextMenu = (
+    outerRef: React.MutableRefObject<null>,
+    onSetOpen: (open: boolean) => void,
+): ContextMenuPosition => {
     const [xPos, setXPos] = useState('0px');
     const [yPos, setYPos] = useState('0px');
 
     const handleContextMenu = useCallback(
         event => {
             event.preventDefault();
-            if (outerRef && outerRef.current.contains(event.target)) {
+            // eslint-disable-next-line
+            // @ts-ignore
+            if (outerRef && outerRef.current && outerRef.current.contains(event.target)) {
                 setXPos(`${event.pageX}px`);
                 setYPos(`${event.pageY}px`);
                 onSetOpen && onSetOpen(true);
